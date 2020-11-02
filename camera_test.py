78082bef7a0c92daab64d7476ec056de28368d56
str5490 <str5490@gmail.com>
2020-11-02 21:58:09 +0900

    camera test

---
+++ camera_test.py
@@ -0,0 +1,17 @@
+import cv2
+
+cap = cv2.VideoCapture(0) # 0 or -1
+
+while cap.isOpened():
+	ret, img = cap.read()
+
+	if ret:
+		cv2.imshow('camera-o', img)
+		if cv2.waitKey(1) & 0xFF == 27: #esc
+			break
+	else:
+		print('no camera!')
+		break
+
+cap.release()
+cv2.destroyAllWindows()
